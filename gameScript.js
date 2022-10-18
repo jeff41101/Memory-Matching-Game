edb2e75108444f7483ec7f96cee0c7f1c326aa61
jeff41101 <jeff41101@yahoo.com.tw>
2022-10-18 18:14:07 +0800

    [gameScript.js][GameStyle.css][Adjust the game]

--- gameScript.js
+++ gameScript.js
@@ -150,7 +150,8 @@ function change(x) {
           setTimeout(function() {
               //$("#ol").html(`<center><div id="iol"><h2>Congrats!</h2><p style="font-size:23px;padding:10px;">You completed the ${mode} mode in ${moves} moves. It took you ${time}.</p><p style="font-size:18px">Comment Your Score!<br/>Play Again ?</p><button onclick="start(3, 4)">3 x 4</button> <button onclick="start(4, 4)" style="w">4 x 4</button><button onclick="start(4, 5)">4 x 5</button><button onclick="start(5, 6)">5 x 6</button><button onclick="start(6, 6)">6 x 6</button></div></center>`);
               $("#ol").html(`<center><div id="iol"><h2>Congrats!</h2><p style="font-size:23px;padding:10px;">You completed the ${mode} mode in ${moves} moves. It took you ${time}.</p><p style="font-size:18px">Comment Your Score!<br/>Play Again ?</p><button onclick="start(3, 4)">3 x 4</button> <button onclick="start(4, 4)" style="w">4 x 4</button><button onclick="start(4, 5)">4 x 5</button>`);
-              window.parent.postMessage(moves , '*');
+              var stats = { moves: moves, time: time };
+              window.parent.postMessage(stats, '*');
               $("#ol").fadeIn(750);
           }, 1500);
     }
