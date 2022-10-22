1c516723570c736b1b2d9d53a4bb2990794f7661
jeff41101 <jeff41101@yahoo.com.tw>
2022-10-22 10:32:53 +0800

    webgame push

--- gameScript.js
+++ gameScript.js
@@ -43,8 +43,8 @@ var data1;
 
 $(document).ready(function () {
     $.getJSON("data.json", function (data) {
-        console.log(data.name); // Prints: Harry
-        console.log(data.age); // Prints: 14
+        data1 = data;
+        console.log(data);
     }).fail(function () {
         console.log("An error has occurred.");
     });
