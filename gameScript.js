5d1823c63523b2f94b4aa1261b565ae11ad0045a
jeff41101 <jeff41101@yahoo.com.tw>
2022-10-23 13:06:24 +0800

    webgame push

--- gameScript.js
+++ gameScript.js
@@ -98,7 +98,7 @@ function start(r,l) {
                         <div class='front'></div>
                         <div class='back container'><img class="image" id ="img${n}" src=${items[n - 1]}>
                             <div class="middle">
-                                <div class="text"><pre>id = ${json[itemNumbers[n]]}    name = ${json[itemNumbers[n]]}</pre></div>
+                                <div class="text"><pre>id = ${json[0]}    name = ${json[itemNumbers[n]]}</pre></div>
                             </div>
                         </div>
                     </div>
