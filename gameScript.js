c481c0983b130a8dc9f1f221ab71532513cbc38d
jeff41101 <jeff41101@yahoo.com.tw>
2022-10-23 19:08:02 +0800

    webgame push

--- gameScript.js
+++ gameScript.js
@@ -104,7 +104,7 @@ function start(r, l) {
             console.log(typeof itemNumbers);
             console.log('m =' +m);
             console.log(typeof m);
-            console.log(mydata[parseInt(m, 10)]);
+            console.log(mydata[m.toString()]);
             console.log(mydata[1]);
             //$("table").append(`<td id='${n}' onclick="change(${n})"><div class='inner'><div class='front'></div><div class='back'><p>${items[n - 1]}</p></div></div></td>`);
             $("table").append(
